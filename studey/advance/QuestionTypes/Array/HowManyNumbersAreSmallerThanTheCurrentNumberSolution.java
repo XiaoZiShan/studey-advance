a80cc832616665cff10aaa314569ad50e817fbe1
JRebel 🌋 <oncwnuDg-YFycSdj35RCDs4oIOgk@git.weixin.qq.com>
2020-05-16 19:00:33 +0800

    🏓 完善ing

---
+++ studey/advance/QuestionTypes/Array/HowManyNumbersAreSmallerThanTheCurrentNumberSolution.java
@@ -0,0 +1,11 @@
+package advance.QuestionTypes.Array;
+
+/**
+ * Created by 刷题使我快乐,自律使我自由 !
+ */
+public class HowManyNumbersAreSmallerThanTheCurrentNumberSolution  {
+
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+        return null;
+    }
+}
