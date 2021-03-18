9575ef2d08066d9e35abc69e0498e46ce63ce8fd
XiaoZiShan <todo@qq.com>
2021-03-19 00:21:17 +0800

    增加多对多树的show 数据以及单元测试

--- src/main/java/studey/advance/datastructure/tree/MultNode.java
+++ src/main/java/studey/advance/datastructure/tree/MultNode.java
@@ -1,9 +1,52 @@
 package studey.advance.datastructure.tree;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import studey.advance.datastructure.pojo.PermissionResource;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by 刷题使我快乐,自律使我自由 !
- * 一对多权限Node 多树
+ * 多对多用户-角色-权限树
  */
+@Data
+@AllArgsConstructor
 public class MultNode {
+    private Integer id;
+    private String userName;
+    private List<RoleNode> roleNode;
+
+
+    @Data
+    public static class RoleNode{
+        private Integer id;
+        private String name;
+        private List<Integer> parentIds;
+        private Boolean areParent;
+        private PermissionNode permission;
+        private List<RoleNode> children = new ArrayList<>();
+
+        public RoleNode(Integer id, String name, List<Integer> parentIds, Boolean areParent, PermissionNode permission) {
+            this.id = id;
+            this.name = name;
+            this.parentIds = parentIds;
+            this.areParent = areParent;
+            this.permission = permission;
+        }
+    }
+
+
+    @Data
+    public static class PermissionNode extends SoloNode{
+        private List<Integer> parentIds;
+
+        public PermissionNode(List<Integer> parentIds, Integer id, Integer permissionLevelEnum, Integer parentId, Boolean areParent, PermissionResource permissionResource) {
+            super(id, permissionLevelEnum, parentId, areParent, permissionResource);
+            this.setParentId(parentId);
+        }
+    }
 
 }
+
---
+++ src/main/java/studey/advance/questiontypes/tree/MultTreeCompleteSolution.java
@@ -0,0 +1,14 @@
+package studey.advance.questiontypes.tree;
+
+import studey.advance.datastructure.tree.MultNode;
+
+import java.util.List;
+
+/**
+ * Created by 刷题使我快乐,自律使我自由 !
+ */
+public class MultTreeCompleteSolution {
+    protected List<MultNode> viewCompleteMultTree(List<MultNode> multNodes, List<MultNode.RoleNode> roleNodes ,List<MultNode.PermissionNode> permissionNodes){
+        return null;
+    }
+}
