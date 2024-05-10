8b2b60b8ed216ff271b54505edf9ea756b15b8bc
Jasper <todo@example.com>
2024-05-10 18:57:34 +0800

    wait 1

--- src/test/java/advance/leetcode/questiontypes/array/MajorityElementSolutionTest.java
+++ src/test/java/advance/leetcode/questiontypes/array/MajorityElementSolutionTest.java
@@ -8,11 +8,32 @@ import studey.advance.leetcode.questiontypes.array.MajorityElementSolution;
 public class MajorityElementSolutionTest extends MajorityElementSolution{
     @Test
     void majorityElementByMyOneTest(){
-        int[] arrayOneCase = {2,2,1,1,1,2,2};
-        int arrayOneCaseResulte = majorityElementByMyOne(arrayOneCase);
+        int[] arrayOneCase = {2,2,1,1,1,2,2,5};
+        int arrayOneCaseResulte = super.majorityElementByMyOne(arrayOneCase);
         Assertions.assertEquals(2,arrayOneCaseResulte);
         int[] arrayTwoCase = {3,2,3};
-        int arrayTwoCaseResulte = majorityElementByMyOne(arrayTwoCase);
+        int arrayTwoCaseResulte = super.majorityElementByMyOne(arrayTwoCase);
+        Assertions.assertEquals(3,arrayTwoCaseResulte);
+    }
+
+    @Test
+    void majorityElementByMyTwoTest(){
+        int[] arrayOneCase = {2,2,1,1,1,2,2,5};
+        int arrayOneCaseResulte = super.majorityElementByMyTwo(arrayOneCase);
+        Assertions.assertEquals(2,arrayOneCaseResulte);
+        int[] arrayTwoCase = {3,2,3};
+        int arrayTwoCaseResulte = super.majorityElementByMyTwo(arrayTwoCase);
+        Assertions.assertEquals(3,arrayTwoCaseResulte);
+    }
+    
+    
+    @Test
+    void majorityElementByExpertOneTest(){
+        int[] arrayOneCase = {2,2,1,1,1,2,2,5};
+        int arrayOneCaseResulte = super.majorityElementByExpertOne(arrayOneCase);
+        Assertions.assertEquals(2,arrayOneCaseResulte);
+        int[] arrayTwoCase = {3,2,3};
+        int arrayTwoCaseResulte = super.majorityElementByExpertOne(arrayTwoCase);
         Assertions.assertEquals(3,arrayTwoCaseResulte);
     }
 }
