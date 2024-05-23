90206073b737c910cde2ad6914e131e58c2f8046
Jasper <todo@example.com>
2024-05-23 18:47:40 +0800

    wait 3

--- src/test/java/advance/leetcode/questiontypes/array/MergeSortedArraySolutionTest.java
+++ src/test/java/advance/leetcode/questiontypes/array/MergeSortedArraySolutionTest.java
@@ -5,17 +5,17 @@ import studey.advance.leetcode.questiontypes.array.MergeSortedArraySolution;;
 
 public class MergeSortedArraySolutionTest extends MergeSortedArraySolution{
     
-    // @Test
-    // void mergeSortedArrayByMyOneTest(){
-    //     int[] nums1 = {1,2,3,0,0,0};
-    //     int[] nums2 = {2,5,6};
-    //     int[] target = {1,2,2,3,5,6};
-    //     super.mergeSortedArrayByMyOne(nums1, 3, nums2, nums2.length, target);
-    //     int[] nums1Null = {1};
-    //     int[] nums2Null = {};
-    //     int[] targetNull = {1};
-    //     super.mergeSortedArrayByMyOne(nums1Null, 1, nums2Null, 0, targetNull);
-    // }
+    @Test
+    void mergeSortedArrayByMyOneTest(){
+        int[] nums1 = {1,2,3,0,0,0};
+        int[] nums2 = {2,5,6};
+        int[] target = {1,2,2,3,5,6};
+        super.mergeSortedArrayByMyOneFailed(nums1, 3, nums2, nums2.length, target);
+        int[] nums1Null = {1};
+        int[] nums2Null = {};
+        int[] targetNull = {1};
+        super.mergeSortedArrayByMyOneFailed(nums1Null, 1, nums2Null, 0, targetNull);
+    }
 
     @Test
     void mergeSortedArrayByMyTwoTest(){
@@ -41,4 +41,16 @@ public class MergeSortedArraySolutionTest extends MergeSortedArraySolution{
         super.mergeSortedArrayByExpertOne(nums1Null, 1, nums2Null, 0, targetNull);
     }
 
+
+    @Test
+    void mergeSortedArrayByExpertTwoTest(){
+        int[] nums1 = {1,2,3,0,0,0};
+        int[] nums2 = {2,5,6};
+        int[] target = {1,2,2,3,5,6};
+        super.mergeSortedArrayByExpertTwo(nums1, 3, nums2, nums2.length, target);
+        int[] nums1Null = {1};
+        int[] nums2Null = {};
+        int[] targetNull = {1};
+        super.mergeSortedArrayByExpertTwo(nums1Null, 1, nums2Null, 0, targetNull);
+    }
 }
